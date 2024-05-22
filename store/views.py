3b14a10a9ac5e20112f5e6e8eb926b3c48ac9979
pedro <pedrocole1416@gmail.com>
2024-05-21 18:10:19 -0600

    Act calificaciones de productos

--- store/views.py
+++ store/views.py
@@ -7,6 +7,7 @@ from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.db.models import Q
 from .forms import ReviewForm
 from django.contrib import messages
+from orders.models import OrderProduct
 # Create your views here.
 
 def store(request, category_slug= None):
@@ -42,9 +43,22 @@ def product_detail(request, category_slug, product_slug):
     except Exception as e:
         raise e
     
+    if request.user.is_authenticated:
+        try:
+            orderproduct = OrderProduct.objects.filter(user= request.user, product_id= single_product.id).exists()
+        except OrderProduct.DoesNotExist:
+            orderproduct = None
+    else:
+        orderproduct = None
+        
+    reviews = ReviewRating.objects.filter(product_id= single_product.id, status= True)
+    
+    
     context = {
         'single_product': single_product,
         'in_cart': in_cart,
+        'orderproduct': orderproduct,
+        'reviews': reviews
     }
     return render(request, 'store/product_detail.html', context)
 
